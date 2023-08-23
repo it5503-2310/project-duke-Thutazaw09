fe1ea6f58bae549e787d371c7f7f82fa27541ce1
Thutazaw09 <zeyophite545@gmail.com>
2023-08-24 02:08:35 +0630

    update cases

--- config.py
+++ config.py
@@ -9,6 +9,7 @@ mark_1_symbol = "X"
 
 emptyDescription_msg = "OOPS!!! The description of a todo cannot be empty"
 bye_msg = "Bye. Hope to see you again soon!"
-falseInstrection = "OOPS!!! I'm sorry, but I don't know what that means :("
+falseCommand = "OOPS!!! I'm sorry, but I don't know what that means :("
 list_msg = "Here are the tasks in your list:"
 delete_msg = "Noted. I've removed this task:"
+taskOk_msg = "Got it. I've added this task:"
--- looping.py
+++ looping.py
@@ -1,10 +1,30 @@
 import end
+import config
+
 
 def Looping(cmd:str):
-    
+    count = 0
     while cmd != "bye":
-        print("in the loop")
+        print(config.dotted)
         
+        break_task = cmd.split(" ")
+        
+        if len(break_task) == 0:
+            print(config.falseCommand)
+            
+        
+        elif break_task[0] == "todo":
+            if len(break_task) < 1:
+                print(config.emptyDescription_msg)
+            else:
+                print(config.taskOk_msg)
+                count += 1
+             
+        else:
+            print(config.falseCommand)
+                
+        cmd = str(input("> "))
+    
     end.end()    
     
     
