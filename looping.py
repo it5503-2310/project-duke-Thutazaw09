f9698543b80484a1dde6015f7697bf18d5ac64c0
Thutazaw09 <zeyophite545@gmail.com>
2023-08-28 01:33:12 +0630

    Task is modified

--- looping.py
+++ looping.py
@@ -26,13 +26,80 @@ def Looping(cmd:str):
                 list_of_task.append(t) # type: ignore
                 id = list_of_task.index(t) # type: ignore
                 t.setId(id)
-
-               
+                print(t)
+                print("Now you have {} tasks in the list.".format(len(list_of_task)-1))
         
-             
+        elif break_task[0] == "deadline":
+            if len(break_task) < 5:
+                print(config.missing_arg_msg)
+                continue
+            else:
+                print(config.taskOk_msg)
+                d = task.Task(break_task[0])
+                d.setTaskName(break_task[1])
+                d.setTaskCatagory(break_task[2])
+                d.set_DeadLine(break_task[3],break_task[4])
+                list_of_task.append(d) # type: ignore
+                id = list_of_task.index(d) # type: ignore
+                d.setId(id)
+                print(d)
+                print("Now you have {} tasks in the list.".format(len(list_of_task)-1))
+                
+        
+        elif break_task[0] == "event":
+            if len(break_task) < 8:
+                print(config.missing_arg_msg)
+                continue
+            else:
+                print(config.taskOk_msg)
+                e = task.Task(break_task[0])
+                e.setTaskName(break_task[1])
+                e.setTaskCatagory(break_task[2])
+                e.set_start_and_end(break_task[3],break_task[4],break_task[5],break_task[6],break_task[7])
+                list_of_task.append(e) # type: ignore
+                id = list_of_task.index(e) # type: ignore
+                e.setId(id)
+                print(e)
+                print("Now you have {} tasks in the list.".format(len(list_of_task)-1))
+        
+        elif break_task[0] == "list":
+            if len(list_of_task) <= 1:
+                print(config.list_msg)
+                continue
+            else:
+                for i in range(1,len(list_of_task)):
+                    print(i,"." + str(list_of_task[i]))# type: ignore 
+        
+        elif break_task[0] == "mark":
+            if len(list_of_task) <= 1:
+                continue
+            id = int(break_task[1])
+            task.markObj(list_of_task[id])  # type: ignore
+            print(config.marked_msg)
+            print(list_of_task[id])
+            
+        elif break_task[0] == "unmark":
+            if len(list_of_task) <= 1:
+                continue
+            id = int(break_task[1])
+            task.unmarkObj(list_of_task[id])  # type: ignore
+            print(config.unmark_msg)
+            print(list_of_task[id])
+            
+        elif break_task[0] == "find":
+            if len(list_of_task) <= 1:
+                continue
+            for i in range(1,len(list_of_task)):
+                if list_of_task[i].taskCatagory == break_task[1]: # type: ignore
+                    print(list_of_task[i])
+            
+        elif break_task[0] == "delete": # update the the whole database not efficient at all should use with hashcode
+            pass
+         
         else:
             print(config.falseCommand)
 
+       
         print(" ")
         print(config.dotted)  
         print(" ")      
--- task.py
+++ task.py
@@ -1,4 +1,4 @@
-
+import config
 class Task():
 
     def __init__(self,command:str) -> None:
@@ -6,9 +6,10 @@ class Task():
         self.taskId = id
         self.taskName = None
         self.taskCatagory = None
-        self.deadLine = None
-        self.event = None
         self.marked = False
+        self.deadLine = None
+        self.plan = None
+      
 
     def setTaskName(self,task:str) -> None:
         self.taskName = task
@@ -19,11 +20,34 @@ class Task():
     def setId(self,newId:int) -> None:
         self.taskId = newId
 
-
-    def __str__(self) -> str:
-        return str(self.taskName)
-
-  
+    def __str__(self) -> str: 
+        if self.command == "todo":
+            return "    [{}][{}] {} {}".format(config.todo_symbol,config.mark_symbol if self.marked else " ",self.taskName,self.taskCatagory)
+        elif self.command == "deadline":
+            return "    [{}][{}] {} {} {}".format(config.deadline_symbol,config.mark_symbol if self.marked else " ",self.taskName,self.taskCatagory,self.deadLine)
+        elif self.command == "event":
+            return "    [{}][{}] {} {} {}".format(config.event_symbol,config.mark_symbol if self.marked else " ",self.taskName,self.taskCatagory,self.plan)
+        else:
+            return " Error "
+        
+    def set_DeadLine(self,artical:str,duedate:str)-> None:
+        art = artical[1:]
+        self.deadLine = "("+art+": "+duedate+")"
+    
+    def set_start_and_end(self,art1:str,day:str,time_1:str,art2:str,time_2:str):
+        a1 = art1[1:]
+        a2 = art2[1:]
+        self.plan = "("+a1+": "+day+" "+time_1+" "+a2+": "+time_2+")"
+        
         
 
+def markObj(obj:Task)->None:
+    obj.marked = True
+        
+def unmarkObj(obj:Task)->None:
+    obj.marked = False
+        
 
+# data structure fixing is required  
+# delete 
+# type problem needed to fix
